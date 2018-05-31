3023ee050e8b3f6914faf88bc952b020942bcda0
Stephen Royle <stephenjroyle@gmail.com>
2018-05-31 20:50:15 +0100

    booleans tests completed

--- src/booleans.js
+++ src/booleans.js
@@ -1,61 +1,77 @@
 const negate = (a) => {
-  // your code here
+  return !a
 };
 
 const both = (a, b) => {
-  // your code here
+  return a && b
 };
 
 const either = (a, b) => {
-  // your code here
+  return a || b
 };
 
 const none = (a, b) => {
-  // your code here
+  return !(a||b)
 };
 
 const one = (a, b) => {
-  // your code here
+  return (a === true && b === false) || (a === false && b === true)
 };
 
 const truthiness = (a) => {
-  // your code here
+  return !!a
 };
 
 const isEqual = (a, b) => {
-  // your code here
+  return a === b
 };
 
 const isGreaterThan = (a, b) => {
-  // your code here
+  return a > b
 };
 
 const isLessThanOrEqualTo = (a, b) => {
-  // your code here
+  return a <= b
 };
 
 const isOdd = (a) => {
-  // your code here
+  if (a % 2 === 1) {
+    return true
+  } else {
+    return false
+  }
 };
 
 const isEven = (a) => {
-  // your code here
+  if (a % 2 === 0) {
+    return true
+  } else {
+    return false
+  }
 };
 
 const isSquare = (a) => {
-  // your code here
+  if (a === 0) {
+    return true
+  } else {
+  return a > 0 && Math.sqrt(a) % 1 === 0
+  }
 };
 
 const startsWith = (char, string) => {
-  // your code here
+  if (char === string.charAt(0)) {
+    return true
+  } else {
+    return false
+  }
 };
 
 const containsVowels = (string) => {
-  // your code here
+  return string.toLowerCase().includes("a") || string.toLowerCase().includes("e") || string.toLowerCase().includes("i") || string.toLowerCase().includes("o") || string.toLowerCase().includes("u")
 };
 
 const isLowerCase = (string) => {
-  // your code here
+  return string === string.toLowerCase()
 };
 
 module.exports = {
